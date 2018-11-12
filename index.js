b427c3a64d10b6e8e41e20e32efdb9de1da5d2d1
marcin-pasiewicz <marcin.pasiewicz@gmail.com>
2018-11-12 15:18:06 +0100

    add retreving headers from request

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ const server = http.createServer(function(req, res) {
     const trimmedPath = path.replace(/^\/+|\/+$/g,'');
     const queryStringObject = parsedUrl.query;
     const method = req.method.toLocaleLowerCase();
+    const headers = req.headers;
+
     res.end('Hello world\n')
-    console.log('Request received on path: ' + trimmedPath + ' with method: ' + method + ' with these query sting parameters ', queryStringObject)
+    console.log('Request received with these headers ', headers)
 });
 server.listen(3000, function () {
     console.log('server is listening on port 3000 now')
