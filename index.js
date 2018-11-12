f36c92d1f18b90c0a4bf16a97d9421aab6e3d529
marcin-pasiewicz <marcin.pasiewicz@gmail.com>
2018-11-12 15:33:27 +0100

    add retreving payload

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 const server = http.createServer(function(req, res) {
     const parsedUrl = url.parse(req.url, true);
     const path = parsedUrl.pathname;
@@ -7,10 +8,19 @@ const server = http.createServer(function(req, res) {
     const queryStringObject = parsedUrl.query;
     const method = req.method.toLocaleLowerCase();
     const headers = req.headers;
+    const decoder = new StringDecoder('utf-8');
+    let buffer = '';
+    req.on('data', function (data) {
+        buffer += decoder.write(data)
+    });
+    req.on('end', function () {
+        buffer += decoder.end();
 
-    res.end('Hello world\n')
-    console.log('Request received with these headers ', headers)
+        res.end('Hello world\n')
+        console.log('Request received with this payload ', buffer)
+    });
 });
+
 server.listen(3000, function () {
     console.log('server is listening on port 3000 now')
 });
