e1985b5730d2906883bc51cf43a7abb868e31ed8
marcin-pasiewicz <marcin.pasiewicz@gmail.com>
2018-11-24 14:34:21 +0100

    add checks workers

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const server = require('./lib/server');
+const workers = require('./lib/workers');
 
 const app = {};
 
 app.init = function () {
     server.init();
+    workers.init();
 };
 
 app.init();
