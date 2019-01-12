9cd2d5949d24f8f771c0da75ff5e09c12afaccbd
marcin-pasiewicz <marcin.pasiewicz@gmail.com>
2019-01-12 13:37:15 +0100

    add test runner

---
+++ test/index.js
@@ -0,0 +1,118 @@
+const assert = require('assert');
+const helpers = require('./../lib/helpers');
+
+_app = {};
+
+_app.tests = {
+    'unit': {}
+};
+
+_app.tests.unit['helpers.getANumber should return a number'] = function (done) {
+    const val = helpers.getANumber();
+    assert.equal(typeof (val), 'number');
+    done()
+};
+
+_app.tests.unit['helpers.getANumber should return number 1'] = function (done) {
+    const val = helpers.getANumber();
+    assert.equal(val, 1);
+    done()
+};
+
+_app.tests.unit['helpers.getANumber should return number 2'] = function (done) {
+    const val = helpers.getANumber();
+    assert.equal(val, 2);
+    done()
+};
+
+
+_app.countTests = function() {
+
+};
+
+_app.produceTestsReport = function(limit, successes, errors) {
+    console.log('');
+    console.log('-----------------BEGIN TEST REPORT-----------------');
+    console.log('');
+    console.log('Total:', limit);
+    console.log('Passed:', successes);
+    console.log('Failed:', errors.length);
+    console.log('');
+
+    if(errors.length > 0) {
+        console.log('-----------------BEGIN ERROR DETAILS-----------------');
+        console.log('');
+
+        errors.forEach(function (testError) {
+            console.log('\x1b[31m%s\x1b[0m', testError.name );
+            console.log(testError.error);
+            console.log('');
+        });
+
+        console.log('-----------------END ERROR DETAILS-----------------');
+    }
+    console.log('');
+    console.log('-----------------END TEST REPORT-----------------');
+};
+
+_app.countTests = function() {
+    let counter = 0;
+
+    for(let key in _app.tests) {
+        if(_app.tests.hasOwnProperty(key)) {
+            const subTest = _app.tests[key];
+
+            for(let testName in subTest) {
+                if(subTest.hasOwnProperty(testName)) {
+                    counter++
+                }
+            }
+        }
+    }
+    return counter
+};
+
+_app.runTest = function() {
+    const errors = [];
+    let successes = 0;
+    let counter = 0;
+    const limit = _app.countTests();
+
+    for(let key in _app.tests) {
+        if(_app.tests.hasOwnProperty(key)) {
+            const subTest = _app.tests[key];
+
+            for(let testName in subTest) {
+                if(subTest.hasOwnProperty(testName)) {
+                    (function () {
+                        let tmpTestName = testName;
+                        let testValue = subTest[testName];
+
+                        try {
+                            testValue(function () {
+                                console.log('\x1b[32m%s\x1b[0m', tmpTestName );
+                                counter++;
+                                successes++;
+                                if(counter === limit) {
+                                    _app.produceTestsReport(limit, successes, errors)
+                                }
+                            })
+                        } catch (e) {
+                            errors.push({
+                                'name': testName,
+                                'error': e
+                            });
+                            console.log('\x1b[31m%s\x1b[0m', tmpTestName );
+                            counter++;
+                            if(counter === limit) {
+                                _app.produceTestsReport(limit, successes, errors)
+                            }
+                        }
+                    })()
+                }
+            }
+        }
+    }
+};
+
+_app.runTest();
